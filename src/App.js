bf483773bb82d1284f2d6f751c5c7760520a22ce
Yash Rai <yashraizb@gmail.com>
2025-01-20 23:26:36 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Yash Rai's <code>DEMO</code> app.
         </p>
         <a
           className="App-link"
